e4c4c196cc4d160184e322c3cdc07cca44feeccf
amanchaurasiya783 <127719170+amanchaurasiya783@users.noreply.github.com>
2023-08-06 13:29:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //your JS code here. If required.
 let removeBtn = document.getElementById("removeBtn");
-let select = document.getElementByid("colorSelect");
-colorSelect.addEventListener('click', ()=>{
-	colorSelect.remove(colorSelect.selectedIndex);
-})
+let select = document.getElementById("colorSelect");
+
+function removeOption(){
+	select.remove(select.selectedIndex);
+}
